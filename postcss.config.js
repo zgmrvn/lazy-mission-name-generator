851d02a3224f175ba947eb533499082cf6988da9
zgmrvn <zgmrvn@gmail.com>
2019-11-02 16:09:26 +0100

    Relace double quotes with single

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,19 @@
 const tailwindcss = require('tailwindcss')
 const autoprefixer = require('autoprefixer')
-const purgecss = require("@fullhuman/postcss-purgecss")
+const purgecss = require('@fullhuman/postcss-purgecss')
 
 const plugins = [
   tailwindcss,
   autoprefixer
 ];
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   plugins.push(
     purgecss({
       content: [
-        "./src/**/*.vue"
+        './src/**/*.vue'
       ],
-      whitelist: ["html", "body"]
+      whitelist: ['html', 'body']
     })
   )
 }
